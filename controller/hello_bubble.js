817e446e3f720f65d66801684998060b5618486a
dytwest <dytwest@163.com>
2015-06-05 01:22:42 +0800

    no message

--- controller/hello_bubble.js
+++ controller/hello_bubble.js
@@ -7,8 +7,6 @@ var Userlength = 0;//用户总数
 var Bubblelength = 0;
 var start = false;//开始
 
-var User_ip_port = [];
-
 //生成随机的Bubble
 function randomBubble(){
     for(var i = Bubblelength; i<100; i++)
@@ -18,7 +16,7 @@ function randomBubble(){
         randomBubble_queue[i].y = Math.floor(Math.random() * globle_height);
         randomBubble_queue[i].r = 5;
         randomBubble_queue[i].score = 1;
-        randomBubble_queue[i].eaten = false;
+        randomBubble_queue[i].type = 0;
     }
     Bubblelength = 100;
     console.log(randomBubble_queue);
@@ -39,17 +37,16 @@ function eat(){
         }
         for(var j = i+1; j<User_queue.length; j++)
         {
-
             if(User_queue[i].r > User_queue[j].r) {
                 if (distance(User_queue[i], User_queue[j]) < User_queue[i].r - User_queue[j].r) {
                     checkstate(User_queue[i], User_queue[j]);
-                    User_queue.splice(j, 1);
+                    User_queue[j].eaten = true;
                 }
             }
             else if(User_queue[i].r < User_queue[j].r) {
                 if (distance(User_queue[j], User_queue[i]) < User_queue[j].r - User_queue[i].r) {
                     checkstate(User_queue[j], User_queue[i]);
-                    User_queue.splice(i, 1);
+                    User_queue.live = false;
                 }
             }
         }
@@ -74,6 +71,7 @@ function checkstate(Bubblebig,Bubblesmall)
     Bubblesmall.eaten = true;
     Bubblebig.r = Math.sqrt(Bubblebig.r*Bubblebig.r + Bubblesmall.r*Bubblesmall.r);
     Bubblebig.v = 4000 / (Bubblebig.r * Bubblebig.r);
+    Bubblebig.allscore += Bubblesmall.score;
     Bubblebig.score += Bubblesmall.score;
 }
 
@@ -98,7 +96,7 @@ function splite(Bubble){
     var sy = Bubble.vecy - Bubble.y;
     var x = Bubble.x + 2*Bubble.r * (sx / Math.sqrt(sx*sx + sy*sy));
     var y = Bubble.y + 2*Bubble.r * (sy / Math.sqrt(sx*sx + sy*sy));
-    addBubble(User_queue.length, x, y, Bubble.id, Bubble.vecx, Bubble.vecy, speed*3, Bubble.dividecount, score);
+    addBubble(User_queue.length, x, y, Bubble.id, Bubble.vecx, Bubble.vecy, speed*3, Bubble.dividecount, score,Bubble.allscore);
 }
 
 //添加bubble
@@ -112,11 +110,13 @@ function addBubble(length){
     User_queue[length].vecx = 0;
     User_queue[length].vecy = 0;
     User_queue[length].eaten = false;
+    User_queue[length].allscore = allscore;
     User_queue[length].score = 0;
-    User_queue[length].sub = new Array();
     User_queue[length].dividecount = 0;
     User_queue[length].name = "string";
     User_queue[length].finish = false;
+    User_queue[length].start = false;
+    User_queue[length].type = 1;
 }
 
 function addBubble(pos, x, y, r, id, vecx, vecy, v, dc, score)
@@ -130,10 +130,12 @@ function addBubble(pos, x, y, r, id, vecx, vecy, v, dc, score)
     User_queue[pos].vecx = vecx;
     User_queue[pos].vecy = vecy;
     User_queue[pos].eaten = false;
+    User_queue[pos].allscore = 0;
     User_queue[pos].score = score;
     User_queue[pos].dividecount = dc;
     User_queue[pos].name = "string";
     User_queue[pos].finish = false;
+    User_queue[pos].type = 1;
 }
 //初始化用户
 function initUser(){
@@ -146,7 +148,7 @@ function initUser(){
         //某用户退出,或者分裂
         for (var i = 0; i < User_queue.length; i++) {
             if (User_queue[i].finish)
-                User_queue[i].splice(i, 1);
+                User_queue.splice(i, 1);
             if(User_queue[i].dividecount != 0)
                 splite(User_queue[i]);
         }
@@ -154,8 +156,31 @@ function initUser(){
         move();
         //检查是否被吃
         eat();
+        //检查是否有需要重新开始的用户
+        restart();
     }
     console.log(User_queue[Userlength-1]);
+}
+
+function restart()
+{
+    for(var i = 0; i< User_queue.length; i++)
+    {
+        if(User_queue[i].dividecount == 0)
+        {
+            if(User_queue[i].eaten){}
+        }
+        else
+        {
+            var live = Math.pow(2, User_queue[i].dividecount);
+            for(var j = 0; j<User_queue.length && User_queue[j].id == User_queue[i].id; j++)
+            {
+                if(User_queue[j].eaten)
+                    live--;
+            }
+            if(live == 0){}
+        }
+    }
 }
 
 //开启服务器
