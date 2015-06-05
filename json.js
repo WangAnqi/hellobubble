7833342fa6d684904aa2006afc727ec5c0f97c26
qianjun <jy8239778@126.com>
2015-06-05 16:05:28 +0800

    no message

--- json.js
+++ json.js
@@ -4,7 +4,7 @@
 //start game
 {"type":1, "data":{"id":10, "name":"dogedoge"}}
 //bubble's action
-{"type":2, "data":{"id":510, "direction":"?","mouse":"w"}}
+{"type":2, "data":{"id":510, "directionx":10,"directionx":200,"keydown":true}}
 //close the game
 {"type":3, "data":{"id":203}}
 
